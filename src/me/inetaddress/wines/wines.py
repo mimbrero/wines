11599169a350a8d7bfa09d870ca0313873d35442
Alberto Mimbrero <albertosm2003@gmail.com>
2021-10-29 22:13:59 +0200

    Create parse_file(str) function

--- src/me/inetaddress/wines/wines.py
+++ src/me/inetaddress/wines/wines.py
@@ -4,9 +4,32 @@ Created on 29 oct 2021
 @author: alberto
 '''
 from collections import namedtuple
+from datetime import date
+import csv
+from me.inetaddress.wines.util.parsing_utils import parse_date
 
 # --------------------------------------
 # ENTREGA 1
 # --------------------------------------
 
 Wine = namedtuple("Wine", "name, country, region, winery, rating, number_of_ratings, price, since, origin_appellation")
+
+def parse_file(path:str) -> [Wine(str, str, str, str, float, int, float, date, bool)]:
+    '''
+    Abre el archivo que se sitúa en la ruta dada como parámetro y lo parsea en una lista de tuplas de tipo Wine.
+    El archivo debe estar codificado en utf-8.
+    
+    @param path: la ruta del archivo a parsear
+    @return una lista de tuplas de tipo Wine con los valores del archivo
+    '''
+    with open(path, encoding="utf-8") as file:
+        reader = csv.reader(file)
+        next(reader) # Pasamos la línea 0, que solo es la cabecera con la descripción de las columnas
+        
+        to_return = []
+        for name, country, region, winery, rating, number_of_ratings, price, since, origin_appellation in reader:
+            wine = Wine(name, country, region, winery, float(rating), int(number_of_ratings), float(price), 
+                        parse_date(since), bool(origin_appellation))
+            to_return.append(wine)
+        
+        return to_return

e34caa1a4c82c4c34750d639a426935ec36e7c06
Alberto Mimbrero <albertosm2003@gmail.com>
2021-10-29 22:13:36 +0200

    Create wines.py

---
+++ src/me/inetaddress/wines/wines.py
@@ -0,0 +1,12 @@
+'''
+Created on 29 oct 2021
+
+@author: alberto
+'''
+from collections import namedtuple
+
+# --------------------------------------
+# ENTREGA 1
+# --------------------------------------
+
+Wine = namedtuple("Wine", "name, country, region, winery, rating, number_of_ratings, price, since, origin_appellation")

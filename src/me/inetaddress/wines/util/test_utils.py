c988fc38644a2164fec00b12038e96d0c9ed1df3
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2021-11-20 01:02:53 +0100

    Create #print_sequence

--- src/me/inetaddress/wines/util/test_utils.py
+++ src/me/inetaddress/wines/util/test_utils.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Sequence
 
 
 def print_test_header(test_name: str) -> None:
@@ -8,5 +8,20 @@ def print_test_header(test_name: str) -> None:
 
 
 def print_iterable(iterable: Iterable, before: str = " ", after: str = "") -> None:
-    for i in iterable:
-        print(f"{before}- {i}{after}")
+    """
+    Prints one by one the elements of the given iterable.
+    """
+    for item in iterable:
+        print(f"{before}- {item}{after}")
+
+
+def print_sequence(sequence: Sequence, before: str = " ", after: str = "") -> None:
+    """
+    Prints one by one the elements of the given sequence (an ORDERED (tuple, list...) iterable).
+    """
+    if not sequence:  # If it's empty
+        print(f"{before}The given list is empty!")
+        return
+
+    for i, item in enumerate(sequence):
+        print(f"{before}[{i}] {item}{after}")

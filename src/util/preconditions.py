8fd85e4fff149f7488e636b7c05440576241c847
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2021-12-27 21:21:46 +0100

    Documentate #check_value

--- src/util/preconditions.py
+++ src/util/preconditions.py
@@ -1,3 +1,9 @@
 def check_value(condition, message):
+    """
+    Raises a ValueError with the given message if the given condition is not True.
+
+    @param condition: condition to check if it's True
+    @param message: message to raise the ValueError
+    """
     if not condition:
         raise ValueError(message)

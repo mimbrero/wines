5a927429d817de698d8ad62d9b42f91d639dcb0e
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2021-12-27 20:48:58 +0100

    Create parsing_utils#parse_bool

--- src/util/parsing_utils.py
+++ src/util/parsing_utils.py
@@ -1,6 +1,16 @@
 from datetime import datetime, date
 
 
+def parse_bool(to_parse: str) -> bool:
+    """
+    Parsea un bool dado en tipo str.
+
+    @param to_parse: bool a parsear
+    @return bool parseado
+    """
+    return eval(to_parse.capitalize())
+
+
 def parse_date(to_parse: str) -> date:
     """
     Parsea una fecha dada en tipo str con formato dd/mm/aaaa.
